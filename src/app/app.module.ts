f9c39982dd9653a49eb85578db84ecdbefbe3362
nitin5599 <nitin5599>
2021-04-16 00:34:25 +0530

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,38 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { ContactComponent } from './contact/contact.component';
+import { FeaturesComponent } from './features/features.component';
+import { HardwareComponent } from './hardware/hardware.component';
+import { EcommComponent } from './ecomm/ecomm.component';
+import { PricingComponent } from './pricing/pricing.component';
+import { MoreComponent } from './more/more.component';
+import { FooterComponent } from './footer/footer.component';
+import { BlogComponent } from './blog/blog.component';
+import { FeedbackComponent } from './feedback/feedback.component';
+import { FaqComponent } from './faq/faq.component';
+import { HowItWorksComponent } from './how-it-works/how-it-works.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    LoginComponent,
+    NavbarComponent,
+    ContactComponent,
+    FeaturesComponent,
+    HardwareComponent,
+    EcommComponent,
+    PricingComponent,
+    MoreComponent,
+    FooterComponent,
+    BlogComponent,
+    FeedbackComponent,
+    FaqComponent,
+    HowItWorksComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/ecomm/ecomm.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-ecomm',
+  templateUrl: './ecomm.component.html',
+  styleUrls: ['./ecomm.component.css']
+})
+export class EcommComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
